8510e4a8c7d497a3d884576ea505eb2cd8769ecc
Philipp Allgeuer <philipp.allgeuer@uni-hamburg.de>
2022-01-19 12:09:32 +0100

    Finalise opencv-python

--- install-opencv-python-3.4.17-cuda-10.1.sh
+++ install-opencv-python-3.4.17-cuda-10.1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -i
 # Install OpenCV python into a conda environment
+# Note: On Ubuntu 20.04 you may need to launch with CFG_OPENCV_STRICT=0 CFG_OPENCV_CMAKE="-DWITH_TBB=OFF"
 
 # Retrieve the script directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -7,7 +8,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Configuration
 export CFG_CUDA_VERSION=10.1
 export CFG_OPENCV_PYTHON_TAG=61
-export CFG_OPENCV_STRICT=0
 export CFG_CONDA_PYTHON=3.9
 
 # Run the main installation script
