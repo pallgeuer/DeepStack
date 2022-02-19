7870386b995c83ea69b670cd1da611e9430c4ca6
Philipp Allgeuer <philipp.allgeuer@gmail.com>
2022-02-20 00:59:31 +0100

    Add tensorrt variables

--- install-pytorch-1.10.2-cuda-10.2.sh
+++ install-pytorch-1.10.2-cuda-10.2.sh
@@ -9,6 +9,8 @@ export CFG_CUDA_VERSION=10.2
 export CFG_PYTORCH_TAG=v1.10.2
 export CFG_TORCHVISION_TAG=v0.11.3
 export CFG_OPENCV_TAG=4.5.5
+export CFG_TENSORRT_VERSION=7.0.0
+export CFG_TENSORRT_URL='https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/7.0/7.0.0.11/tars/TensorRT-7.0.0.11.Ubuntu-18.04.x86_64-gnu.cuda-10.2.cudnn7.6.tar.gz'
 export CFG_CONDA_PYTHON=3.9
 
 # Run the main installation script
