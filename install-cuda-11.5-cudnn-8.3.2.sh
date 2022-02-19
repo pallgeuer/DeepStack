c8920bc6d7619736d5081fcc9c1933625c6b1fb7
Philipp Allgeuer <philipp.allgeuer@gmail.com>
2022-02-19 22:38:31 +0100

    Revamp CUDA configurations to match up better with supported TensorRT

--- install-cuda-11.5-cudnn-8.3.2.sh
+++ install-cuda-11.5-cudnn-8.3.2.sh
@@ -8,8 +8,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export CFG_MAX_GCC_VERSION=11
 export CFG_CUDA_VERSION=11.5
 export CFG_CUDA_URL='https://developer.download.nvidia.com/compute/cuda/11.5.1/local_installers/cuda_11.5.1_495.29.05_linux.run'
-export CFG_CUDNN_VERSION=8.3.1
-export CFG_CUDNN_URL='https://developer.nvidia.com/compute/cudnn/secure/8.3.1/local_installers/11.5/cudnn-linux-x86_64-8.3.1.22_cuda11.5-archive.tar.xz'
+export CFG_CUDNN_VERSION=8.3.2
+export CFG_CUDNN_URL='https://developer.nvidia.com/compute/cudnn/secure/8.3.2/local_installers/11.5/cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive.tar.xz'
 
 # Run the main installation script
 "$SCRIPT_DIR/install-cuda.sh"
