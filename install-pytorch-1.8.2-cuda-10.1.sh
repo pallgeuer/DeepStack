efed6291ddc6d71857fc1a70bc9b693b632f3243
Philipp Allgeuer <philipp.allgeuer@gmail.com>
2022-02-20 00:59:15 +0100

    Bump opencv

--- install-pytorch-1.8.2-cuda-10.1.sh
+++ install-pytorch-1.8.2-cuda-10.1.sh
@@ -9,7 +9,7 @@ export CFG_CUDA_VERSION=10.1
 export CFG_PYTORCH_TAG=v1.8.2
 export CFG_TORCHVISION_TAG=v0.9.2-rc2
 export CFG_TORCHVISION_VERSION=0.9.2
-export CFG_OPENCV_TAG=4.5.4
+export CFG_OPENCV_TAG=4.5.5
 export CFG_CONDA_PYTHON=3.9
 
 # Run the main installation script
