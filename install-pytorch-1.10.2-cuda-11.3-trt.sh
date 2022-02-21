d32571088b587e310b0a8ea36ae284753de2de4b
Philipp Allgeuer <philipp.allgeuer@gmail.com>
2022-02-21 11:34:00 +0100

    Separate trt vs non-trt pytorch installers

--- install-pytorch-1.10.2-cuda-11.3-trt.sh
+++ install-pytorch-1.10.2-cuda-11.3-trt.sh
@@ -5,10 +5,12 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Configuration
-export CFG_CUDA_VERSION=10.2
+export CFG_CUDA_VERSION=11.3
 export CFG_PYTORCH_TAG=v1.10.2
 export CFG_TORCHVISION_TAG=v0.11.3
 export CFG_OPENCV_TAG=4.5.5
+export CFG_TENSORRT_VERSION=8.2.3
+export CFG_TENSORRT_URL='https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.2.3.0/tars/TensorRT-8.2.3.0.Linux.x86_64-gnu.cuda-11.4.cudnn8.2.tar.gz'
 export CFG_CONDA_PYTHON=3.9
 
 # Run the main installation script
