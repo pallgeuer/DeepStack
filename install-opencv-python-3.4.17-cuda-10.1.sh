3741e0b7c75250cf5b21b20d658dad7b67ba9fe5
Philipp Allgeuer <philipp.allgeuer@gmail.com>
2022-04-07 17:05:06 +0200

    Bump opencv-python tags

--- install-opencv-python-3.4.17-cuda-10.1.sh
+++ install-opencv-python-3.4.17-cuda-10.1.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Configuration
 export CFG_CUDA_VERSION=10.1
-export CFG_OPENCV_PYTHON_TAG=61
+export CFG_OPENCV_PYTHON_TAG=63
 export CFG_CONDA_PYTHON=3.9
 
 # Run the main installation script
