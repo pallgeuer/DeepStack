25faf6205340bf82808ca7b9350c62495242a9a1
Philipp Allgeuer <philipp.allgeuer@gmail.com>
2022-04-10 17:45:33 +0200

    Adjust opencv python 4 tag

--- install-opencv-python-4.5.5-cuda-10.2.sh
+++ install-opencv-python-4.5.5-cuda-10.2.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Configuration
 export CFG_CUDA_VERSION=10.2
-export CFG_OPENCV_PYTHON_TAG=64
+export CFG_OPENCV_PYTHON_TAG=8f2049e  # Slightly ahead of 64 in order to include some compilation fixes
 export CFG_CONDA_PYTHON=3.9
 
 # Run the main installation script
