8e47d90c936a4da9090ca1d9a10d95b5f1f77966
Philipp Allgeuer <philipp.allgeuer@gmail.com>
2024-10-06 18:29:00 +0200

    CUDA install

---
+++ install-cuda-11.4-cudnn-8.6.0.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Install an isolated CUDA/cuDNN stack
+
+# Retrieve the script directory
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Configuration
+export CFG_MAX_GCC_VERSION=11
+export CFG_CUDA_VERSION=11.4
+export CFG_CUDA_URL='https://developer.download.nvidia.com/compute/cuda/11.4.4/local_installers/cuda_11.4.4_470.82.01_linux.run'
+export CFG_CUDA_SAMPLES_TAG=v11.4.1
+export CFG_CUDNN_VERSION=8.6.0
+export CFG_CUDNN_URL='https://developer.nvidia.com/compute/cudnn/secure/8.6.0/local_installers/11.8/cudnn-linux-x86_64-8.6.0.163_cuda11-archive.tar.xz'
+
+# Run the main installation script
+"$SCRIPT_DIR/install-cuda.sh"
+# EOF
